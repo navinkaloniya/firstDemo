0ac0f8c9472bfe01dc8340322bc4f2f18880a86d
navinkaloniya <navinkaloniya@gmail.com>
2022-08-22 11:10:59 +0530

    put, delete,post

--- src/Services/Employee/employee.service.ts
+++ src/Services/Employee/employee.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable} from '@angular/core'
-import { Observable, of, ReplaySubject } from 'rxjs';
-import { IEmployee } from 'src/Model/Employee/employeeModel'
+import { catchError, Observable, of, ReplaySubject, throwError } from 'rxjs';
+import { IEmployee } from 'src/Model/Employee/employeeModel';
+
 
 //If we want to use the data accross multiple components and the logic then we 
 // need to provide that information in the service and we can reuse it.
@@ -9,7 +10,7 @@ import { IEmployee } from 'src/Model/Employee/employeeModel'
 // IN this case it does not have any dependency so the parameter list is empty
 @Injectable()
 export class EmployeeService {
-    employeeURL:string='api/employees';
+    employeeURL:string='api/employees1';
     employees:any;
     constructor(private _http:HttpClient){
 
@@ -24,7 +25,39 @@ export class EmployeeService {
     // ];
 
     getEmployeeByApi():Observable<IEmployee[]>{
-       return this._http.get<IEmployee[]>(this.employeeURL);
+       return this._http.get<IEmployee[]>(this.employeeURL)       
+       .pipe(catchError(this.handleError));
+    }
+
+    createEmployeebyApi(emp:IEmployee){
+        console.log(emp);
+        this._http.post<IEmployee>(this.employeeURL,emp,
+            {
+                headers:new HttpHeaders({
+                    'Content-Type':'application/json'
+                })
+            })
+            .pipe(catchError(this.handleError));
+
+    }
+    updateEmployeebyApi(emp:IEmployee){
+        //http://localhost:4200/employee/1
+        console.log(emp);
+        this._http.put<IEmployee>(`${this.employeeURL}/${emp.code}`,emp,
+            {
+                headers:new HttpHeaders({
+                    'Content-Type':'application/json'
+                })
+            })
+            .pipe(catchError(this.handleError));
+
+    }
+    DeleteEmployeebyApi(code:string){
+        //http://localhost:4200/employee/1
+        
+        this._http.delete(`${this.employeeURL}/${code}`)
+            .pipe(catchError(this.handleError));
+
     }
 
     // getEmployees():IEmployee[] {
@@ -43,4 +76,39 @@ export class EmployeeService {
     });
 return subject;
     }
+
+    private handleError(errorResponse:HttpErrorResponse){
+         console.log(errorResponse);
+        // console.log(errorResponse.status);
+        // console.log(errorResponse.url);
+        if(errorResponse.error  instanceof ErrorEvent)
+        {
+            console.log(errorResponse.message);
+            // Client side error
+        }
+        else
+        {
+            console.log(errorResponse.message);
+            // server side
+        }
+        //return new ErrorObservable('')
+      return throwError(()=>{
+        return 'Something wrong occur!!';
+      });
+      
+
+    }
 }
+
+//Employee          - Get the list of all employee 
+//Employee/update/1 - They will update the data for the record having an id of 1.
+                      // The data that you want to update sent it into the body
+                      // {FirstName:'',LastName:'',Gender:''}
+//Employee/delete/1 - They will delete the record with id 1
+//Employee/Create   - Sent the data to us into the body and we will create the record into the DB
+
+//Post- It will always create an entity into the Database
+// Url- {code:'',FirstName:''}
+
+//Put - It will create an entity into the Database and if matched it will update the entity
+// Url- {code:'',FirstName:''}
